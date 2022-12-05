59369410bbc1bebd46fab5c0802fb55ca6c163cc
ksiciarz1 <kacpersiciarz2001@gmail.com>
2022-12-05 19:10:24 +0100

    Created Checkers project

---
+++ CheckersGame/GridPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame
+{
+    internal struct GridPosition
+    {
+        public int x;
+        public int y;
+
+        public GridPosition()
+        {
+            x = 0;
+            y = 0;
+        }
+        public GridPosition(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public GridPosition(GridPosition gridPosition)
+        {
+            x = gridPosition.x;
+            y = gridPosition.y;
+        }
+    }
+}
